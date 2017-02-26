ad0f7cd6a3630c25a808ead8c117f7c1a40eeb2d
Денис Хлякин <mail4aka@gmail.com>
2017-02-26 11:18:50 +0300

    Fix logger

--- src/main/java/persistent/db/DBConnection.java
+++ src/main/java/persistent/db/DBConnection.java
@@ -7,11 +7,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static DBConnection ourInstance = new DBConnection();
     private static Logger logger = Logger.getLogger(DBConnection.class);
+    private static DBConnection ourInstance = new DBConnection();
     private Connection con;
 
     private DBConnection() {
+        logger.info("Connecting to DB");
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
